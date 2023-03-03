e2ffd93e2d1b184c7859cf076f149f58276b3f22
hamid.achahboune <hamid.achahboune@gmail.com>
2023-03-03 16:41:43 +0100

    heaser premier partie

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/css/style.header.css">
+
+    <title>Kitsutheque</title>
+</head>
+<header>
+    <div id="head">
+        <a href="#"><img src="./asset/img/logo-bibliotheque.svg" alt="logo-kitsutheque" class="logo" height="80px"></a>
+        <span class="titre-logo">Kitsutheque</span>
+
+        <a href="#"><img src="./asset/img/icons-connexion.png"  class="connexion" alt="connexion"></a>
+
+        <a href="#"><img src="./asset/img/icons8-livre-ouvert-50.png" class="cataloge" alt="livre"></a>
+
+            <form action=""  id="nav-bar">
+                <input class="" type ="text" placeholder="Rechercher votre manga"/>
+                <img  class ="loup" src="./asset/img/search-engine-icon_icon-icons.com_55728.png" alt="loup" >
+            </form>
+        
+    </div>
+    
+
+</header>
+<body>
+    
+</body>
+</html>
