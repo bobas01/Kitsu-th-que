3b11fdc2268b6876afa922c20a9493bed7942303
hamid.achahboune <hamid.achahboune@gmail.com>
2023-03-05 23:13:25 +0100

    first comit heaser+footer

--- header.php
+++ header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,25 +9,66 @@
 
     <title>Kitsutheque</title>
 </head>
-<header>
-    <div id="head">
-        <a href="#"><img src="./asset/img/logo-bibliotheque.svg" alt="logo-kitsutheque" class="logo" height="80px"></a>
-        <span class="titre-logo">Kitsutheque</span>
 
-        <a href="#"><img src="./asset/img/icons-connexion.png"  class="connexion" alt="connexion"></a>
-
-        <a href="#"><img src="./asset/img/icons8-livre-ouvert-50.png" class="cataloge" alt="livre"></a>
-
-            <form action=""  id="nav-bar">
-                <input class="" type ="text" placeholder="Rechercher votre manga"/>
-                <img  class ="loup" src="./asset/img/search-engine-icon_icon-icons.com_55728.png" alt="loup" >
-            </form>
-        
-    </div>
-    
-
-</header>
 <body>
-    
+    <header>
+        <div id="container">
+
+            <a href="#"><img src="./asset/img/logo-bibliotheque.svg" alt="logo-kitsutheque" class="logo" height="80px"></a>
+            <span class="titre-logo">Kitsutheque</span>
+
+            <a href="#"><img src="./asset/img/icons-connexion.png" class="connexion" alt="connexion"></a>
+
+            <a href="#"><img src="./asset/img/icons8-livre-ouvert-50.png" class="cataloge" alt="livre"></a>
+
+            <div class="search">
+                <form action="">
+                    <input class="search-bar" type="text" placeholder="Rechercher votre manga" />
+                    <button class="button" type="submit"><img src="./asset/img/icon-search.png" alt="icon loupe"></button>
+                </form>
+            </div>
+        </div>
+
+
+    </header>
+    <section>
+
+    </section>
+
+    <footer>
+    <div class="information">
+        <div class="adress">
+            <p>Adresse:<br>
+                65 rue Malandin<br>
+                01000 Peronnas<br>
+                Téléphone:<br>
+                06.06.06.06.06
+            </p>
+        </div> 
+        <div class="service">
+            <p>Accessibilité:<br>
+                Bus n°2<br>
+                Parking<br>
+                Accès handicapé
+            </p>
+        </div>
+        <div class="horaires">
+            <p>Horaires<br>
+                Lundi au Vendredi : 10h00-18h00<br>
+                Samedi : 8h-12h<br>
+                Dimanche : fermée
+            </p>
+        </div>
+        <div class="réseaux-sociaux">
+        <p>Réseaux sociaux:</p>
+        <a href="#"><img src="./asset/img/icons8-facebook.svg" alt=""></a>
+        <a href="#"><img src="./asset/img/icons8-instagram.svg" alt=""></a>
+        <a href="#"><img src="./asset/img/icons8-twitter.svg" alt="" ></a>
+
+        </div>
+    </div>
+</footer>
+
 </body>
+
 </html>
