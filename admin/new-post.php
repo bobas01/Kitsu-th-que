8e981ef45182e669948446530b9289e938a03998
Taysma <walesca.telemaq@gmail.com>
2023-03-07 16:57:19 +0100

    inscription terminée(2)

--- admin/new-post.php
+++ admin/new-post.php
@@ -1,7 +1,9 @@
 <?php
 include_once '../connexion.php';
+include_once './header-admin.php';
 
 if (isset($_POST['submit'])) {
+    /* Ajout de nouveau livre */
     $genre = addslashes($_POST['genre']);
     $public = addslashes($_POST['public']);
     $title = addslashes($_POST['title']);
@@ -12,8 +14,14 @@ if (isset($_POST['submit'])) {
     $cover = 'image.jpg';
     $extract = addslashes($_POST['extract']);
 
-    $sql = "INSERT INTO `manga`( `id_genre`, `id_public`, `title`, `volume`, `editor`, `published_at`, `author`, `cover`, `extract` )VALUES ('$genre', '$public', '$title', '$volume', '$editor', '$published_at', '$author', '$cover', '$extract')";
-    $db->query($sql);
+    $manga = "INSERT INTO `manga`( `id_genre`, `id_public`, `title`, `volume`, `editor`, `published_at`, `author`, `cover`, `extract` )VALUES ('$genre', '$public', '$title', '$volume', '$editor', '$published_at', '$author', '$cover', '$extract')";
+    $db->query($manga);
+
+    /* Ajout de catégories */
+    $id_category = addslashes($_POST['id_category']);
+
+    $category = "INSERT INTO `manga_category`(`id_manga`, `id_category`)VALUES (LAST_INSERT_ID(), '$id_category')";
+    $db->query($category);
 
     $_SESSION['sucess'] = "Produit ajouté avec succès !";
     header('Location: ./list.php');
@@ -49,14 +57,26 @@ if (isset($_POST['submit'])) {
                 <label for="author">Auteur</label><br>
                 <input type="author" name="author" id="author" maxlength="50">
                 <!--<label for="cover">Page de couverture</label>
-                <input type="file" name="cover" id="cover">
-                <label for="category">Catégories</label>
-                <input type="text" name="category" id="category">-->
+                <input type="file" name="cover" id="cover">-->
                 <br>
                 <label for="extract">Résumé</label><br>
-                <textarea name="extract" id="extract" cols="100" rows="5" maxlength="255"></textarea>
+                <textarea name="extract" id="extract" cols="50" rows="5" maxlength="255"></textarea>
             </div>
+            <fieldset>
+                <legend>Catégories</legend>
+                <div>
+                    <?php
+                    $sql2 = "SELECT `id`,`name`,`slug` FROM `category`";
+                    $req = $db->query($sql2);
+                    while ($category = $req->fetch(PDO::FETCH_ASSOC)) { ?>
+                        <input type="checkbox" id="id_category" name="id_category">
+                        <label for="<?= $category['slug'] ?>"><?= $category['name'] ?></label><br>
+                    <?php } ?>
+                    <br>
+                </div>
+            </fieldset>
         </fieldset>
+
         <fieldset id="btn">
             <legend>Post/Reset</legend>
             <input type="submit" name="submit" value="Post ">
@@ -64,6 +84,8 @@ if (isset($_POST['submit'])) {
         </fieldset>
     </form>
 </div>
+
+
 </main>
 
 </body>
