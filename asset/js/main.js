300789a83208e8aca84a9673977b86393957054c
Sequeira Alexandre <67326944+bobas01@users.noreply.github.com>
2023-03-15 20:42:30 +0100

    ajout js

--- asset/js/main.js
+++ asset/js/main.js
@@ -92,7 +92,22 @@ function handleTouchMove(evt) {
    yDown = null;
 }
 
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+const connexion = document.getElementById('connexion')
 
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
+
+connexion.addEventListener('click', function(){
+   container.classList.toggle('active');
+})
 
 
 
