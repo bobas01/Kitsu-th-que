cd2c9f6e1ec9b20b59b01684f52bba73ae36df54
Sequeira Alexandre <67326944+bobas01@users.noreply.github.com>
2023-06-13 14:43:26 +0200

    connexion modifier

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO ('mysql:host=localhost;dbname=kitsutheque;charset=utf8','root');
+    $db = new PDO ('mysql:host=srv936.hstgr.io;dbname=u715151447_kitsutheque;charset=utf8','u715151447_bobas', 'Equinox01!');
 }
 catch(PDOException $e){
     echo "Erreur :" . $e->getMessage();
--- model/auth.php
+++ model/auth.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once '../connexion.php';
+include_once '../connexion.php';
 
 $pseudo = $_POST['pseudo'];
 $pass = $_POST['password'];
